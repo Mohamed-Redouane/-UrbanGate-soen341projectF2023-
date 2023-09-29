2e7f04dd493cad99606c91f9b14f24664e729ce4
Anthony El-Khoury <136773346+Toterino@users.noreply.github.com>
2023-09-28 22:44:38 -0400

    Update App.tsx

--- UrbanGate/Frontend/src/assets/Components/App.tsx
+++ UrbanGate/Frontend/src/assets/Components/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
-function App() {
+function App() { //functional component, look at documenation
 
   return (
-    <>
+    <> <!--the function can only return one tag, so whole text below has to be envelopped by 1 tag-->
       <div>
         <p> Welcome to this real estate website  </p>
       </div>
