9f9c9dc17362f2ec0687d1a36c59787e27a131ff
Anthony El-Khoury <136773346+Toterino@users.noreply.github.com>
2023-09-28 22:23:43 -0400

    Update main.tsx

--- UrbanGate/Frontend/src/main.tsx
+++ UrbanGate/Frontend/src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom"; // from React Router's documentation
 import App from './assets/Components/App.tsx'
-import Header from './assets/Components/Header.tsx'
+import Header from './assets/Components/Header.tsx' // from React's documentation
 import Footer from'./assets/Components/Footer.tsx'
 import Houses from './assets/Components/Houses.tsx'
 
-const router = createBrowserRouter([{path: "/", element: <App/>,},
+const router = createBrowserRouter([{path: "/", element: <App/>,}, // from React Router's documentation
 {path: "/houses", element: <Houses/>}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 
     <Header/>
-    <RouterProvider router = {router}/>
+    <RouterProvider router = {router}/> <!--from React Router's documentation -->
     <Footer/>
 
   </React.StrictMode>
