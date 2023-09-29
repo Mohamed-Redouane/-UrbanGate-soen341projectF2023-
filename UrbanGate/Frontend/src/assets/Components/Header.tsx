54d7bda798d492a36ff74c38d525adb5674f383c
Anthony El-Khoury <136773346+Toterino@users.noreply.github.com>
2023-09-28 22:39:48 -0400

    Update Header.tsx

--- UrbanGate/Frontend/src/assets/Components/Header.tsx
+++ UrbanGate/Frontend/src/assets/Components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
         <div style={{position: "static"}}> 
             <div className="row"> <div className="card"> <div className="card-body" style={{backgroundColor: "#01AEB5", textAlign: "center"}}> <h5 className="card-header" style={{backgroundColor: "white"}}>  UrbanGate  </h5>   </div> </div> </div>
         <div className="row"> 
-        <div className="col-3" style={{textAlign: "center", border: "solid", backgroundColor: "white"}}> <a href="/"> <h5 style={{color: "black"}}> About  </h5> </a> </div> <!-- Style attribute is weird for React, look at documenation-->
+        <div className="col-3" style={{textAlign: "center", border: "solid", backgroundColor: "white"}}> <a href="/"> <h5 style={{color: "black"}}> About  </h5> </a> </div> <!-- Style attribute is weird for React, look at https://www.w3schools.com/react/react_css.asp-->
         <div className="col-3" style={{textAlign: "center", border: "solid", backgroundColor: "white"}}> <a href="/houses"> <h5 style={{color: "black"}}> Houses </h5> </a> </div>
         <div className="col-3" style={{textAlign: "center", border: "solid", backgroundColor: "white"}}> <a href="/broker">  <h5 style={{color: "black"}}>Broker </h5> </a> </div>
         <div className="col-3" style={{textAlign: "center", border: "solid", backgroundColor: "white"}}>  <a href="/account"> <h5 style={{color: "black"}}> Account </h5>  </a> </div>
