aed30d1325252fcddc86cc20415e1c663fedf429
Mohamed-Redouane <126030228+Mohamed-Redouane@users.noreply.github.com>
2023-10-01 20:28:59 -0400

    Index.js Update

--- UrbanGate/Backend/index.js
+++ UrbanGate/Backend/index.js
@@ -1,1 +1,21 @@
-// index js for our server
+import express from 'express';
+import connectDB from './utils/mdbconfig.js';
+
+
+
+const app = express();
+
+connectDB().then(() => {
+        console.log("Connected to the database")
+    }).catch((err) => {
+        console.error(err)
+    })
+
+
+const PORT =  3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running  ${PORT}`);
+});
+
