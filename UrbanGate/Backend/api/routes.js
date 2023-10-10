7d00b271bd1848f6222a959640d2dbb40e0bdf3c
Mohamed-Redouane <126030228+Mohamed-Redouane@users.noreply.github.com>
2023-10-10 00:10:11 -0400

    update routes.js file

--- UrbanGate/Backend/api/routes.js
+++ UrbanGate/Backend/api/routes.js
@@ -1,68 +1,10 @@
-const express = require('express');
+import express from 'express';
+import createUser from './controllers/createUser.js';
+import signIn from './controllers/signIn.js';
+
 const router = express.Router();
-import cors from 'cors'
-import USER from './models/users'
-router.use(cors());
 
-        router.get('/homebuyer/search', (req, res) => {
-        
-        });
+router.post('/createUser', createUser);
+router.post('/signIn', signIn);
 
-        router.get('/homebuyer/favorites', (req, res) => {
-        
-
-        });
-
-        router.get('/homebuyer/request-visit', (req, res) => {
-            
-            
-        });
-        router.get('/homebuyer/search-brokers', (req, res) => {
-        
-        });
-        
-        router.get('/homebuyer/mortgage-calculator', (req, res) => {
-            
-        });
-        router.get('/Property_renter/search', (req, res) => {
-       
-        });
-    
-        router.get('/Property_renter/favorites', (req, res) => {
-           
-    
-        });
-    
-        router.get('/Property_renter/request-visit', (req, res) => {
-            
-            
-        });
-        router.get('/Property_renter/search-brokers', (req, res) => {
-           
-        });
-        router.get('/broker/submit-offer', (req, res) => {
-       
-        });
-    
-        router.get('/broker/property-listings', (req, res) => {
-           
-    
-        });
-    
-        router.get('/broker/match-properties', (req, res) => {
-            
-            
-        });
-        router.get('/broker/offer-management', (req, res) => {
-           
-        });
-        
-        router.get('/broker/manage-visits', (req, res) => {
-            
-        });
-
-        router.get('/admin/brokers', (req, res) => {// eg: add remove block 
-            
-        });
-       
-module.exports = router;
+export default router;
