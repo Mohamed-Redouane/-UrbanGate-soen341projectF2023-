bd68c512db1fac632472298dcec486ed3f478064
mylinhtang <mylinhnt99@gmail.com>
2023-10-10 21:41:38 -0400

    updatedMorgageCalculator

--- UrbanGate/Frontend/src/assets/Components/_Houses/MortgageCalculator.tsx
+++ UrbanGate/Frontend/src/assets/Components/_Houses/MortgageCalculator.tsx
@@ -8,27 +8,46 @@ const MortgageCalculator: React.FC = () => {
   const [interestRate, setInterestRate] = useState<number>(0);
   const [term, setTerm] = useState<number>(0);
   const [paymentFrequency, setPaymentFrequency] = useState<number>(12);
-  const [monthlyPayment, setMonthlyPayment] = useState<number | null>(null);
+  const [sentence, setSentence] = useState<string | null>(null); 
+  let monthlyPayment = 0; 
 
   const openModal = () => {
-    setModalIsOpen(true);
+    setModalIsOpen(true); 
   };
 
   const closeModal = () => {
     setModalIsOpen(false);
+    setSentence(" ");
+    resetValues(); 
   };
 
+  const resetValues = () => {
+    setMortgageAmount(0); 
+    setInterestRate(0); 
+    setTerm(0); 
+    setPaymentFrequency(12);
+  }
+
   const calculateMortgage = () => {
+    if (mortgageAmount <= 0 || interestRate <= 0 || term <= 0){
+      alert("Please fill each space with numbers bigger than 0.")
+    }
+    else {
     // Calculate the monthly payment using the formula
     const r = interestRate / 100;
     const n = paymentFrequency;
-    const t = term;
+    const t = term; 
 
     const numerator = mortgageAmount * (r / n);
     const denominator = 1 - Math.pow(1 + r / n, -n * t);
     const mortgagePayment = numerator / denominator;
-
-    setMonthlyPayment(mortgagePayment);
+    
+    monthlyPayment = mortgagePayment; 
+    if (monthlyPayment !== null){
+    let amount = monthlyPayment.toFixed(2); 
+    setSentence("Your monthly payment for a duration of " + term + " year(s) would be: $" + amount + "/month."); 
+    }
+    }
   };
   // style the modal window
   const customStyles = {
@@ -38,31 +57,35 @@ const MortgageCalculator: React.FC = () => {
       right: "auto",
       bottom: "auto",
       transform: "translate(-50%, -50%)",
-      width: "60%",
-      borderRadius: "10px",
-      padding: "20px",
       boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
+      borderRadius: "10px",
+      height:"500px", 
+      backgroundColor:"#e9ecef", 
+      width:"900px",  
     },
   };
+
   return (
-    <div>
+    <div style={{width:"900px"}}>
       <h2>Mortgage Calculator</h2>
       <button className="open-button" onClick={openModal}>
         Open Calculator
       </button>
+    
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        style={customStyles}
         contentLabel="Mortgage Calculator Modal"
+        style={customStyles}
       >
-        <h2>Mortgage Calculator</h2>
-        <form>
+        <h2 className="modal-title">Mortgage Calculator</h2>
+        <small className="prompt">Please enter numerical values to estimate your monthly payment.</small>
+        <form className="modal-form">
           <label>
             Mortgage Amount:
             <input
               type="number"
-              value={mortgageAmount}
+              min="0"
               onChange={(e) => setMortgageAmount(Number(e.target.value))}
             />
           </label>
@@ -70,7 +93,7 @@ const MortgageCalculator: React.FC = () => {
             Interest Rate (%):
             <input
               type="number"
-              value={interestRate}
+              min="0"
               onChange={(e) => setInterestRate(Number(e.target.value))}
             />
           </label>
@@ -78,14 +101,13 @@ const MortgageCalculator: React.FC = () => {
             Term (years):
             <input
               type="number"
-              value={term}
+              min="0"
               onChange={(e) => setTerm(Number(e.target.value))}
             />
           </label>
           <label>
             Payment Frequency:
             <select
-              value={paymentFrequency}
               onChange={(e) => setPaymentFrequency(Number(e.target.value))}
             >
               <option value={12}>Monthly</option>
@@ -93,11 +115,12 @@ const MortgageCalculator: React.FC = () => {
               <option value={52}>Weekly</option>
             </select>
           </label>
+          <br></br>
           <button type="button" onClick={calculateMortgage}>
             Calculate
           </button>
         </form>
-        {monthlyPayment && <p>Monthly Payment: ${monthlyPayment.toFixed(2)}</p>}
+        <p className="sentence-display">{sentence}</p>
       </Modal>
     </div>
   );
