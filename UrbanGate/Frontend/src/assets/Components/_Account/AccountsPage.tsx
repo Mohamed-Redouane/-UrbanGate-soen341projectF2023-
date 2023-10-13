a0c19497a928d194061a1c3d06093d8cce2c2d99
Azmi Abidi <a_abidi@live.concordia.ca>
2023-10-13 17:10:18 -0400

    Design of the brokers page and Fix the sign up form

--- UrbanGate/Frontend/src/assets/Components/_Account/AccountsPage.tsx
+++ UrbanGate/Frontend/src/assets/Components/_Account/AccountsPage.tsx
@@ -1,26 +1,26 @@
-import React from 'react'; 
-import { useState } from 'react';
-import "./SignIn.css"
-import axios from 'axios';
-import {useNavigate} from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import React from "react";
+import { useState } from "react";
+import "./SignIn.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-function AccountsPage(){
-    return(
-      <div className="accountsDiv">
-        <SignIn/>
-        <SignUp/>
-      </div>
-);
+function AccountsPage() {
+  return (
+    <div className="accountsDiv">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 }
 
-function SignIn(){
+function SignIn() {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name,setName]=useState('');
-  const [_,setCookies]=useCookies(["access_token"])
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [_, setCookies] = useCookies(["access_token"]);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -31,43 +31,44 @@ function SignIn(){
   };
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
-  }
+  };
   //const [ ,setCookies]=useCookies(["tokenFromJwt"]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = {email: email, password: password};
-    const responseFromAPI = axios.post("http://localhost:3000/signIn", data)
-    .then((res)=>{
-      alert("success");
-      setCookies("access_token",res.data.token);
-      window.localStorage.setItem("UserID", res.data.userID);
-      navigate("/");
-    })
-    .catch((res)=> alert("failure: " + res));
+    const data = { email: email, password: password };
+    const responseFromAPI = axios
+      .post("http://localhost:3000/signIn", data)
+      .then((res) => {
+        alert("success");
+        setCookies("access_token", res.data.token);
+        window.localStorage.setItem("UserID", res.data.userID);
+        navigate("/");
+      })
+      .catch((res) => alert("failure: " + res));
 
-   //setCookies("tokenFromJwt", responseFromAPI); //going to store the user_id being sent back, this proves authentication and allows user to be verifed
-   //Note going to use cookie to validate whether to re-route sign in page to managing accounts page once authentication token exists
+    //setCookies("tokenFromJwt", responseFromAPI); //going to store the user_id being sent back, this proves authentication and allows user to be verifed
+    //Note going to use cookie to validate whether to re-route sign in page to managing accounts page once authentication token exists
     //navigate("/"); //sends user to home page and the accounts page will not route to account management
-  }
+  };
 
-  return(
-<div className="container">
-  <form onSubmit={handleSubmit}>
-    <h2>Sign In</h2>
-    <div className="fields-container">
-      <label htmlFor = "email">Email </label>
-      <input
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h2>Sign In</h2>
+        <div className="fields-container">
+          <label htmlFor="email">Email </label>
+          <input
             type="email"
             id="email"
             name="email"
             value={email}
-            onChange = {handleEmailChange}
+            onChange={handleEmailChange}
             required
             placeholder="Enter email"
           />
-    </div>
-    <div className="fields-container">
+        </div>
+        <div className="fields-container">
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -93,21 +94,21 @@ function SignIn(){
           />
         </div>
         <button type="submit">Sign In</button>
-  </form>
-</div>
+      </form>
+    </div>
   );
 }
 
-function SignUp(){
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [role, setRole] = useState('');
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
 
   const success = () => {
     alert("SUCCESS");
-  }
-const navigate = useNavigate();
+  };
+  const navigate = useNavigate();
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -118,40 +119,39 @@ const navigate = useNavigate();
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
-  }
+  };
 
-  const handleRoleChange =(e: React.ChangeEvent<HTMLInputElement>) => {
-    setRole(e.target.value);
-  }
-
+  const handleRoleChange = (selectedRole: string) => {
+    setRole((prevRole) => (prevRole === selectedRole ? "" : selectedRole));
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = {email: email, name: name, password: password, role: role};
-    axios.post("http://localhost:3000/createUser", data)
-    .then(()=> alert("SUCCESS"))
-    .catch((res)=> alert("Error code: " + res));
+    const data = { email: email, name: name, password: password, role: role };
+    axios
+      .post("http://localhost:3000/createUser", data)
+      .then(() => alert("SUCCESS"))
+      .catch((res) => alert("Error code: " + res));
     //console.log(email +  "" + password +"" + name+""   + role);
-    
-  }
+  };
 
-return(
-<div className="container">
-  <form onSubmit={handleSubmit}>
-    <h2>Sign Up</h2>
-    <div className="fields-container">
-      <label htmlFor = "email">Email </label>
-      <input
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <div className="fields-container">
+          <label htmlFor="email">Email </label>
+          <input
             type="email"
             id="email"
             name="email"
             value={email}
-            onChange = {handleEmailChange}
+            onChange={handleEmailChange}
             required
             placeholder="Enter email"
           />
-    </div>
-    <div className="fields-container">
+        </div>
+        <div className="fields-container">
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -175,16 +175,36 @@ return(
             placeholder="Enter Name"
           />
         </div>
-         <br/>
-        <span> Homebuyer </span> <input type="radio" value="homebuyer" onChange={handleRoleChange}/> 
-        <span> Broker </span><input type="radio"value="broker" onChange={handleRoleChange}/>
+        <div className="fields-container">
+          <div>
+            <label htmlFor="homebuyer">Homebuyer</label>
+            <input
+              type="radio"
+              id="homebuyer"
+              name="role"
+              value="homebuyer"
+              checked={role === "homebuyer"}
+              onChange={() => handleRoleChange("homebuyer")}
+            />
+            <label htmlFor="broker">Broker</label>
+            <input
+              type="radio"
+              id="broker"
+              name="role"
+              value="broker"
+              checked={role === "broker"}
+              onChange={() => handleRoleChange("broker")}
+            />
+          </div>
+          <div>
+            <button type="button" onClick={() => handleRoleChange("")}>
+              Clear Selection
+            </button>
+          </div>
+        </div>
         <button type="submit">Sign Up</button>
-        
-  </form>
-</div>
+      </form>
+    </div>
   );
 }
-
-
-
 export default AccountsPage;
--- UrbanGate/Frontend/src/assets/Components/_Broker/Broker.tsx
+++ UrbanGate/Frontend/src/assets/Components/_Broker/Broker.tsx
@@ -1,55 +1,69 @@
-import {Link} from 'react-router-dom'
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import axios from "axios";
 import { useState, useEffect } from "react";
+import "./BrokerPage.css";
 
+function Broker() {
+  const [user, checkUser] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
+  const f = async () => {
+    await setLoading(true);
+    await axios
+      .post("http://localhost:3000/checkUser", {
+        userID: window.localStorage.getItem("UserID"),
+      })
+      .then((res) => {
+        console.log("valid");
+        checkUser(res.data);
+      })
+      .catch((res) => {
+        console.log("not valid");
+        checkUser(res.data);
+      });
+    await setLoading(false);
+  };
 
-function Broker () {
-    const [user, checkUser] = useState("");
-    const [isLoading, setLoading] = useState(true);   
+  useEffect(() => {
+    f();
+  }, []);
 
-
-    const f = async () => {
-         await setLoading(true);
-         await axios.post("http://localhost:3000/checkUser", {userID: window.localStorage.getItem("UserID")}).then((res)=>{
-            console.log("valid");
-            checkUser(res.data);
-            
-        }).catch((res)=>{
-            console.log("not valid")
-            checkUser(res.data);
-            
-        });
-         await setLoading(false);
-    };
-
-    
-    useEffect(() => {
-        f();
-    }, []);
-
-
-
-
-
-    
-
-    return (
-  <>
-  {isLoading && <h1>Loading!</h1>}
-  {(!isLoading && (user == "yes")) && <div>
-         <button><Link to="/broker/CreateProperty">Create Property</Link></button>
-         <button><Link to="/broker/ManageProperties">Manage Properties</Link></button>
-     </div>}
- {(!isLoading && (user != "yes")) && <div>
-            <h1>You are not a broker</h1>
-            <img src="https://media.tenor.com/9XrarieeeaAAAAAd/rain-chair.gif" alt="not a broker"></img>
-        </div>}
-  </>
-    )
+  return (
+    <div className="broker-container">
+      {isLoading && <h1>Loading!</h1>}
+      {!isLoading && user === "yes" && (
+        <div>
+          <h1>Welcome, Broker</h1>
+          <p>Here you can access all your broker functions and informations.</p>
+          <div className="broker-buttons">
+            <Link to="/broker/CreateProperty" className="broker-button">
+              Create Property
+            </Link>
+            <Link to="/broker/ManageProperties" className="broker-button">
+              Manage Properties
+            </Link>
+          </div>
+          <div className="broker-background"></div>
+          <div className="broker-text">
+            <h2>Unlock Your Full Potential as a Broker</h2>
+            <p>
+              Join our community of brokers and fulfill your personal and
+              professional dreams.
+            </p>
+            <p>
+              Whether you trade a lot or a little, WE WILL HELP YOU GET AHEAD!
+            </p>
+          </div>
+        </div>
+      )}
+      {!isLoading && user !== "yes" && (
+        <p>
+          This page is reserved to Brokers. Please log in or create a Broker
+          account
+        </p>
+      )}
+    </div>
+  );
 }
-
-
-
 
 export default Broker;
