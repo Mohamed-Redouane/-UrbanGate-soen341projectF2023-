ad682b2f6554b64b3673e2318de4f9ead987e341
Tanzir Hoque <fan042002@gmail.com>
2023-10-24 21:42:25 -0400

    Deletes respective properties upon deleting broker

--- UrbanGate/Backend/api/controllers/deleteBroker.js
+++ UrbanGate/Backend/api/controllers/deleteBroker.js
@@ -1,8 +1,14 @@
 import User from '../models/users.js';
+import Property from '../models/properties.js';
 
 export default async function deleteBroker(req, res) {
     try {
         const { _id } = req.params;
+        const user = await User.findById(_id);
+        const properties = user.ownedProperties;
+        properties.forEach(async p => {
+             await Property.findByIdAndDelete(p._id);
+        });
         await User.deleteOne({_id});
         console.log("USER DELETED");
         return res.status(200).json({popup: "User deleted successfully!",});
