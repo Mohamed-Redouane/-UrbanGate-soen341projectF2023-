acd070ddee9345d2235ec774b146bbc6550e74a4
Azmi Abidi <a_abidi@live.concordia.ca>
2023-11-06 16:18:13 -0500

    Fixing modals and styling
    - fixed all the modals
    - Style create a property page in Broker
    - fix the overflow and shifting in the header

--- UrbanGate/Frontend/src/assets/Components/Header.tsx
+++ UrbanGate/Frontend/src/assets/Components/Header.tsx
@@ -5,56 +5,181 @@
 import "bootstrap/dist/css/bootstrap.min.css"; //[1] don't forget this
 import { motion } from "framer-motion";
 import "./Header.css";
-import logo from "./logo.png" //font from https://www.fontget.com/font/lequire/
-import {useState, useEffect} from 'react'; 
+import logo from "./logo.png"; //font from https://www.fontget.com/font/lequire/
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-function Header() { 
-const[userRole, setUserRole] = useState("");
-const[isSignedIn, setIsSignedIn] = useState(false);
+function Header() {
+  const [userRole, setUserRole] = useState("");
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-const logout = () => {
+  const logout = () => {
     window.localStorage.removeItem("UserID"); //https://www.youtube.com/watch?v=P43DW3HUUH8&t=5957s  at 1:19:52
-}
+  };
 
-const f = () => {
-    axios.post("http://localhost:3000/checkUser", {userID: window.localStorage.getItem("UserID")}) //https://www.youtube.com/watch?v=enOsPhp2Z6Q at 28:12
-    .then((res)=>{
+  const f = () => {
+    axios
+      .post("http://localhost:3000/checkUser", {
+        userID: window.localStorage.getItem("UserID"),
+      }) //https://www.youtube.com/watch?v=enOsPhp2Z6Q at 28:12
+      .then((res) => {
         setUserRole(res.data);
         setIsSignedIn(true);
-        console.log("SIGNED IN")
-    })
-    .catch((err)=>{
+        console.log("SIGNED IN");
+      })
+      .catch((err) => {
         setUserRole("Not Signed In");
         setIsSignedIn(false);
-        console.log(err.response.data.popup)
-    });
-};
+        console.log(err.response.data.popup);
+      });
+  };
 
-useEffect(() => {f();}, []); // Will only be called once on reload https://stackoverflow.com/questions/72824151/react-useeffect-keeps-fetching + https://www.tutorialspoint.com/how-to-call-the-loading-function-with-react-useeffect
-    return (
-        <motion.div initial={{ opacity: 0, scale: 1 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.3 }}>
-            <div className="header-box">
-                <h5 className="title"> <img src={logo} style={{height: "7vw", borderBottomLeftRadius: "10px",borderBottomRightRadius: "10px", filter: "drop-shadow(10px 10px 5px black)"}}/> </h5> {/*https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/drop-shadow*/}
-                <div className="row"> {/* Bootstrap classes + svg icons https://getbootstrap.com/docs/5.3/getting-started/introduction/ */}
-                    <div className="col-2"/>
-                    <div className="col-2"> <a href="/" style={{textDecorationLine: "none" }}> <h5 className="nav-box1"> About<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg></h5> </a> </div> {/*[3], paths made with href*/}
-                    <div className="col-2"> <a href="/houses" style={{ textDecorationLine: "none" }}> <h5 className="nav-box2"> Houses <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="dropd" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg> </h5> </a> </div> {/* [2], style attribute is weird https://www.w3schools.com/react/react_css.asp*/}
-                    <div className="col-2"> <a href="/broker" style={{ textDecorationLine: "none" }}>  <h5 className="nav-box3"> Brokers<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg> </h5> </a> </div>
-                    <div className="col-2"> <a href="/offers" style={{ textDecorationLine: "none" }}>  <h5 className="nav-box3"> Offers<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg> </h5> </a> </div>
-                    <div className="col-2">  <a href="/accounts" style={{ textDecorationLine: "none" }}> 
-
-                    {/*https://www.youtube.com/watch?v=P43DW3HUUH8&t=5957s at 1:19:15*/}
-                     {!window.localStorage.getItem("UserID") ?
-                        (<h5 className="nav-box4"> Accounts<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg> </h5>): 
-                        <button onClick={logout} className="nav-box4"> Logout </button> 
-                     }
-                     {isSignedIn && (<div className="user-type" style={{color: "black"}}>You are signed in as a {userRole}</div>)} {!isSignedIn && (<></>)}
-                    </a> </div>
-                    <div className="col-2"/>
+  useEffect(() => {
+    f();
+  }, []); // Will only be called once on reload https://stackoverflow.com/questions/72824151/react-useeffect-keeps-fetching + https://www.tutorialspoint.com/how-to-call-the-loading-function-with-react-useeffect
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 1 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <div className="header-box">
+        <h5 className="title">
+          {" "}
+          <img
+            src={logo}
+            style={{
+              height: "7vw",
+              borderBottomLeftRadius: "10px",
+              borderBottomRightRadius: "10px",
+              filter: "drop-shadow(10px 10px 5px black)",
+            }}
+          />{" "}
+        </h5>{" "}
+        {/*https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/drop-shadow*/}
+        <div className="row">
+          {" "}
+          {/* Bootstrap classes + svg icons https://getbootstrap.com/docs/5.3/getting-started/introduction/ */}
+          <div className="col-2" />
+          <div className="col-2">
+            {" "}
+            <a href="/" style={{ textDecorationLine: "none" }}>
+              {" "}
+              <h5 className="nav-box-1">
+                {" "}
+                About
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-caret-right-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                </svg>
+              </h5>{" "}
+            </a>{" "}
+          </div>{" "}
+          {/*[3], paths made with href*/}
+          <div className="col-2">
+            {" "}
+            <a href="/houses" style={{ textDecorationLine: "none" }}>
+              {" "}
+              <h5 className="nav-box-2">
+                {" "}
+                Houses{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="dropd"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                </svg>{" "}
+              </h5>{" "}
+            </a>{" "}
+          </div>{" "}
+          {/* [2], style attribute is weird https://www.w3schools.com/react/react_css.asp*/}
+          <div className="col-2">
+            {" "}
+            <a href="/broker" style={{ textDecorationLine: "none" }}>
+              {" "}
+              <h5 className="nav-box-3">
+                {" "}
+                Brokers
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-caret-right-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                </svg>{" "}
+              </h5>{" "}
+            </a>{" "}
+          </div>
+          <div className="col-2">
+            {" "}
+            <a href="/offers" style={{ textDecorationLine: "none" }}>
+              {" "}
+              <h5 className="nav-box-3">
+                {" "}
+                Offers
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-caret-right-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                </svg>{" "}
+              </h5>{" "}
+            </a>{" "}
+          </div>
+          <div className="col-2">
+            {" "}
+            <a href="/accounts" style={{ textDecorationLine: "none" }}>
+              {/*https://www.youtube.com/watch?v=P43DW3HUUH8&t=5957s at 1:19:15*/}
+              {!window.localStorage.getItem("UserID") ? (
+                <h5 className="nav-box-4">
+                  {" "}
+                  Accounts
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-caret-right-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                  </svg>{" "}
+                </h5>
+              ) : (
+                <button onClick={logout} className="nav-box-4">
+                  {" "}
+                  Logout{" "}
+                </button>
+              )}
+              {isSignedIn && (
+                <div className="user-type" style={{ color: "black" }}>
+                  You are signed in as a {userRole}
                 </div>
-            </div>
-        </motion.div>
-    )
+              )}{" "}
+              {!isSignedIn && <></>}
+            </a>{" "}
+          </div>
+          <div className="col-2" />
+        </div>
+      </div>
+    </motion.div>
+  );
 }
-export default Header
+export default Header;
--- UrbanGate/Frontend/src/assets/Components/_Broker/CreateProperty.tsx
+++ UrbanGate/Frontend/src/assets/Components/_Broker/CreateProperty.tsx
@@ -1,48 +1,48 @@
-import {useState} from 'react'
-import axios from 'axios';
-import  Select from 'react-select'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import axios from "axios";
+import Select from "react-select";
+import { toast } from "react-toastify";
+import "./CreatePropertyStyle.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function ManagingProperties() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [types, setTypes] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState("");
+  const [area, setArea] = useState("");
+  const [bedroom, setBedroom] = useState("");
+  const [bathroom, setBathroom] = useState("");
+  const [status, setStatus] = useState("");
+  const [image, setImage] = useState("");
 
-const [title, setTitle] = useState('');
-const [description, setDescription] = useState('');
-const [types,setTypes]=useState('');
-const [location,setLocation]=useState('');
-const [price,setPrice]=useState('');
-const [area,setArea]=useState('');
-const [bedroom,setBedroom]=useState('');
-const [bathroom,setBathroom]=useState('');
-const [status,setStatus]=useState('');
-const [image,setImage]=useState('');
-
-
-const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
   const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDescription(e.target.value);
   };
-  const handleTypesChange = (selectedOption : any) => { //the :any 
+  const handleTypesChange = (selectedOption: any) => {
+    //the :any
     setTypes(selectedOption.value);
   };
-  const handleLocationChange = (selectedOption : any) => { 
+  const handleLocationChange = (selectedOption: any) => {
     setLocation(selectedOption.value);
   };
   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrice(e.target.value);
   };
-  const handleDimensionChange = (selectedOption : any) => { 
+  const handleDimensionChange = (selectedOption: any) => {
     setArea(selectedOption.value);
   };
-  const handleBedroomChange = (selectedOption : any) => { 
+  const handleBedroomChange = (selectedOption: any) => {
     setBedroom(selectedOption.value);
   };
   const handleStatusChange = (selectedOption: any) => {
     setStatus(selectedOption.value);
   };
-  const handleBathroomChange = (selectedOption : any) => { 
+  const handleBathroomChange = (selectedOption: any) => {
     setBathroom(selectedOption.value);
   };
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -50,9 +50,9 @@ const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   };
 
   const bedOptions = [
-    {value: "1 bedroom", label:"1 bed"},
-    {value: "2 bedrooms", label:"2 beds"},
-    {value: "3+ bedrooms", label:"3+ beds"},
+    { value: "1 bedroom", label: "1 bed" },
+    { value: "2 bedrooms", label: "2 beds" },
+    { value: "3+ bedrooms", label: "3+ beds" },
   ];
   const areaOptions = [
     { label: "500-1000 sqft", value: "500-1000 sqft" },
@@ -62,7 +62,7 @@ const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   ];
   const locationOptions = [
     { label: "Downtown", value: "Downtown" },
-    { label: "Griffintown", value: "Griffintown"},
+    { label: "Griffintown", value: "Griffintown" },
     { label: "Mont-Royal", value: "Mont-Royal" },
     { label: "Saint-Laurent", value: "Saint-Laurent" },
     { label: "NDG", value: "NDG" },
@@ -75,137 +75,151 @@ const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     { label: "House", value: "house" },
   ];
   const bathOptions = [
-    {value: "1 bathroom", label:"1 bathroom"},
-    {value: "2 bathrooms", label:"2 bathrooms"},
-    {value: "3+ bathrooms", label:"3+ bathrooms"},
+    { value: "1 bathroom", label: "1 bathroom" },
+    { value: "2 bathrooms", label: "2 bathrooms" },
+    { value: "3+ bathrooms", label: "3+ bathrooms" },
   ];
 
   const statusOptions = [
-    {value: "for_sale", label:"For Sale"},
-    {value: "for_rent", label:"For Rent"},
+    { value: "for_sale", label: "For Sale" },
+    { value: "for_rent", label: "For Rent" },
   ];
 
-  const handleSubmit =  (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const data = {title: title, description: description,image: image, type : types, location: location, price: price, area: area, bedroom: bedroom, bathroom: bathroom, status: status, userID: window.localStorage.getItem("UserID")};
-      axios.post("http://localhost:3000/createProperty", data) //https://www.youtube.com/watch?v=enOsPhp2Z6Q at 28:12
-      .then((res)=>{
+    const data = {
+      title: title,
+      description: description,
+      image: image,
+      type: types,
+      location: location,
+      price: price,
+      area: area,
+      bedroom: bedroom,
+      bathroom: bathroom,
+      status: status,
+      userID: window.localStorage.getItem("UserID"),
+    };
+    axios
+      .post("http://localhost:3000/createProperty", data) //https://www.youtube.com/watch?v=enOsPhp2Z6Q at 28:12
+      .then((res) => {
         toast.success(res.data.popup);
       })
-      .catch((err)=> toast.error(err.response.data.popup)); //you have to write err.response because the data is treated differently than in the .then
-
-  }
-    return (
-        <div className="container">
-            <form onSubmit={handleSubmit}>
-                <h2>Create Property</h2>
-                <div className="fields-container">
-                    <label> Title </label>
-                    <input
-                        type="title"
-                        id="title"
-                        name="title"
-                        value={title}
-                        onChange={handleTitleChange}
-                        required
-                        placeholder="Enter Title"
-                    />
-                </div>
-                <div className="fields-container">
-                    <label> Description </label>
-                    <input
-                        type="description"
-                        id="description"
-                        name="description"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                        required
-                        placeholder="Enter Description"
-                    />
-                </div>
-
-                <div className="fields-container">
-                    <label> Types</label>
-                    <p> {types}</p>
-                    <Select 
-                      options={typeOptions} 
-                      onChange={handleTypesChange}
-                      required
-                      />
-                </div>
-
-                <div className="fields-container">
-                    <label> Location </label>
-                    <Select 
-                        options={locationOptions} 
-                        onChange={handleLocationChange} 
-                        required
-                        />
-                </div>
-
-                <div className="fields-container">
-                    <label htmlFor="quantity">Price:</label>
-                    <p style={{fontSize:"11px", padding:0, margin:0}}> If this property is for rent, enter the price/month</p>
-                    <input type="number"
-                     id="quantity"
-                     name="price"
-                     min="0"
-                    value={price}
-                    onChange={handlePriceChange}
-                    required
-                    placeholder="Price for Sale or Price/month"/>
-                </div>
-
-                <div className="fields-container">
-                    <label htmlFor="quantity">Area</label>
-                    <Select 
-                        options={areaOptions} 
-                        onChange={handleDimensionChange} 
-                        required
-                        />
-                </div>
-
-                <div className="fields-container">
-                    <label htmlFor="quantity">Bedrooms</label>
-                    <Select 
-                    options={bedOptions} 
-                    onChange={handleBedroomChange}
-                    required/>
-                </div>
-
-                <div className="fields-container">
-                    <label htmlFor="quantity">Bathrooms</label>
-                    <Select 
-                    options={bathOptions} 
-                    onChange={handleBathroomChange}
-                    required/>
-                </div>
-
-                <div className="fields-container">
-                    <label> Status:</label>
-                    <Select 
-                      options={statusOptions} 
-                      onChange={handleStatusChange}
-                      required
-                      />
-                </div>
-
-                <div className="fields-container">
-                    <label> Image:</label>
-                    <input 
-                    name="image"
-                    value={image}
-                    onChange={handleImageChange}
-                    required
-                    placeholder="Enter Status"/>
-                </div>
-
-
-                <button type="submit">Create Property</button>
-            </form>
+      .catch((err) => toast.error(err.response.data.popup)); //you have to write err.response because the data is treated differently than in the .then
+  };
+  return (
+    <div className="container" id="CreatePropertyPage">
+      <form onSubmit={handleSubmit}>
+        <h2>Create Property</h2>
+        <div className="fields-container">
+          <label> Title </label>
+          <input
+            type="title"
+            id="title"
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+            required
+            placeholder="Enter Title"
+          />
+        </div>
+        <div className="fields-container">
+          <label> Description </label>
+          <input
+            type="description"
+            id="description"
+            name="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            required
+            placeholder="Enter Description"
+          />
         </div>
 
-    )
+        <div className="fields-container">
+          <label> Types</label>
+          <p> {types}</p>
+          <Select options={typeOptions} onChange={handleTypesChange} required />
+        </div>
+
+        <div className="fields-container">
+          <label> Location </label>
+          <Select
+            options={locationOptions}
+            onChange={handleLocationChange}
+            required
+          />
+        </div>
+
+        <div className="fields-container">
+          <label htmlFor="quantity">Price:</label>
+          <p style={{ fontSize: "11px", padding: 0, margin: 0 }}>
+            {" "}
+            If this property is for rent, enter the price/month
+          </p>
+          <input
+            type="number"
+            id="quantity"
+            name="price"
+            min="0"
+            value={price}
+            onChange={handlePriceChange}
+            required
+            placeholder="Price for Sale or Price/month"
+          />
+        </div>
+
+        <div className="fields-container">
+          <label htmlFor="quantity">Area</label>
+          <Select
+            options={areaOptions}
+            onChange={handleDimensionChange}
+            required
+          />
+        </div>
+
+        <div className="fields-container">
+          <label htmlFor="quantity">Bedrooms</label>
+          <Select
+            options={bedOptions}
+            onChange={handleBedroomChange}
+            required
+          />
+        </div>
+
+        <div className="fields-container">
+          <label htmlFor="quantity">Bathrooms</label>
+          <Select
+            options={bathOptions}
+            onChange={handleBathroomChange}
+            required
+          />
+        </div>
+
+        <div className="fields-container">
+          <label> Status:</label>
+          <Select
+            options={statusOptions}
+            onChange={handleStatusChange}
+            required
+          />
+        </div>
+
+        <div className="fields-container">
+          <label> Image:</label>
+          <input
+            name="image"
+            value={image}
+            onChange={handleImageChange}
+            required
+            placeholder="Enter Status"
+          />
+        </div>
+
+        <button type="submit">Create Property</button>
+      </form>
+    </div>
+  );
 }
 
 export default ManagingProperties;
